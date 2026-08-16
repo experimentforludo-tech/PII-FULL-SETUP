ed9c2abe669d707a9e73c5c6d40c51a0afa95160
experimentforludo-tech <experimentforludo@gmail.com>
2026-08-16 22:13:52 +0530

    Create rateLimiter.js

---
+++ backend/middleware/rateLimiter.js
@@ -0,0 +1,11 @@
+// backend/middleware/rateLimiter.js
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+module.exports = limiter;
